fdf24a3e9ff508ec9b418b13aed3efe950733f35
Egor Churikov <churikov.egor@bk.ru>
2021-05-14 23:26:03 +0300

    Теперь Orders тоже соответсвуют текущему User, а в ProductsOverviewScreen прогружаются вообще все товары, но статусы isFavorite для каждого юзера свои

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -26,9 +26,11 @@ class Products with ChangeNotifier {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
-  Future<void> fetchAndSetProducts() async {
+  Future<void> fetchAndSetProducts([bool filterByUser = false]) async {
+    final filterString =
+        filterByUser ? 'orderBy="creatorId"&equalTo="$userId"' : '';
     var url = Uri.parse(
-        'https://flutter-shop-app-7c5ec-default-rtdb.europe-west1.firebasedatabase.app/products.json?auth=$authToken');
+        'https://flutter-shop-app-7c5ec-default-rtdb.europe-west1.firebasedatabase.app/products.json?auth=$authToken&$filterString');
     try {
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -70,6 +72,7 @@ class Products with ChangeNotifier {
             'description': product.description,
             'imageUrl': product.imageUrl,
             'price': product.price,
+            'creatorId': userId,
           },
         ),
       );
